c0894caf7c884aded24dd3734964daf504ce86d5
Elif Mavis <145778497+mavisellien@users.noreply.github.com>
2024-08-20 13:28:14 +0300

    Add files via upload

---
+++ src/main/java/com/udemy/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.udemy.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "fancy-login";
+    }
+
+    //add access denied
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+        return "access-denied";
+    }
+
+}
